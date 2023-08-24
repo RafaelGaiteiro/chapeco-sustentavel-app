6885d0d5d4a6adca4ed1a7edee09e295cba16417
Rafael Krämer da Silva <rafaelkramerdasilva.dev@gmail.com>
2023-08-24 12:01:15 -0300

    update

--- src/modules/ColetaSeletiva/presentation/components/RegisteredAddress/index.tsx
+++ src/modules/ColetaSeletiva/presentation/components/RegisteredAddress/index.tsx
@@ -1,5 +1,4 @@
-import { Box, Table, TableContainer, Tbody, Td, Text, Tfoot, Th, Thead, Tr } from '@chakra-ui/react';
-import { styled } from 'styled-components';
+import { Box, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
 import { v4 as uuidv4 } from 'uuid';
 import { RegisteredAddressProps } from '../../constants/registeredAddress';
 
@@ -29,58 +28,5 @@ export function RegisteredAddress({ registeredAddress }: RegisteredAddressCompon
 				</Table>
 			</TableContainer>
 		</Box>
-
-		// <AddressContainer>
-		// 	<Text>Endereços Cadastrados</Text>
-		// 	{registeredAddress.map(({ adressName, fullAddress, collectionPeriod }) => (
-		// 		<RegisteredAddressBox key={uuidv4()}>
-		// 			<AddressBox>
-		// 				<AddressName>{adressName}</AddressName>
-		// 				<FullAddress>{collectionPeriod}</FullAddress>
-		// 			</AddressBox>
-		// 			<CollectionTime>{fullAddress}</CollectionTime>
-		// 		</RegisteredAddressBox>
-		// 	))}
-		// </AddressContainer>
 	);
 }
-
-const AddressContainer = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	flex-direction: column;
-	gap: 4px;
-	padding: 16px;
-`;
-
-const RegisteredAddressBox = styled.div`
-	display: grid;
-	grid-template-areas: 'Address CollectionTime';
-	grid-template-columns: 1fr 1fr;
-	padding: 16px;
-	border-width: 1px;
-	border-style: solid;
-	border-radius: 8px;
-	width: 100%;
-	max-width: 600px;
-`;
-
-const AddressBox = styled.div`
-	/* display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: flex-start; */
-`;
-
-const AddressName = styled.h1`
-	font-weight: 600;
-`;
-
-const FullAddress = styled.div`
-	user-select: none;
-`;
-
-const CollectionTime = styled.div`
-	user-select: none;
-`;
